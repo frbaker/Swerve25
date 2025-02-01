a111401e33855c00b4edcb8268227a7eb45b2ec3
frbaker <floyd@floydbaker.com>
2025-02-01 14:00:16 -0600

    with cpp

---
+++ src/main/cpp/subsystems/Pivot.cpp
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+#include <frc/smartdashboard/SmartDashboard.h>
+#include "subsystems/Pivot.h"
+#include "Constants.h"
+
+using namespace PivotConstants;
+
+Pivot::Pivot() {
+  
+}
+
+void Pivot::RunPivot(){
+  m_Pivot.Set(kPivotSpeed);
+}
+
+void Pivot::ReversePivot(){
+  m_Pivot.Set(-kPivotSpeed);
+}
+
+void Pivot::Stop(){
+  m_Pivot.Set(0.0);
+}
+
+
+
+/*bool Arm::RunArm() {
+  // Inline construction of command goes here.
+  // Subsystem::RunOnce implicitly requires `this` subsystem.
+    if (is_arm_up()){
+      m_arm.Set(kArmSpeed);
+      m_arm2.Set(kArmSpeed);
+      frc::SmartDashboard::PutString("Arm_Angle","Up"); 
+      return true;
+    }
+    else {
+      frc::SmartDashboard::PutString("Arm_Angle","Already Loaded");
+      m_arm.Set(0);
+      return false;
+    }
+    
+  //RunOnce creates a command that calls a lambda once, and then finishes.
+}
+
+void Arm::ReverseArm() {
+  // Inline construction of command goes here.
+  // Subsystem::RunOnce implicitly requires `this` subsystem.
+  
+    m_arm.Set(-kArmSpeed);
+    m_arm2.Set(-kArmSpeed);
+    frc::SmartDashboard::PutString("Arm_Angle","Down"); 
+    
+}
+
+void Arm::Stop(){
+  frc::SmartDashboard::PutString("Arm_Angle","notGoing"); 
+  m_arm.Set(0);
+  m_arm2.Set(0);
+}
+
+bool Arm::is_arm_up() {
+  // Query some boolean state, such as a digital sensor.
+  return m_clicker.Get();
+}*/
+
+void Pivot::Periodic() {
+  // Implementation of subsystem periodic method goes here.
+  frc::SmartDashboard::PutString("Arm_Subsystem","Periodic"); 
+}
+
+void Pivot::SimulationPeriodic() {
+  // Implementation of subsystem simulation periodic method goes here.
+}
